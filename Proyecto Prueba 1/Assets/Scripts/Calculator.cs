15de99cd8a1c8a2f1388f09de1f11148f362690a
raymundo_mosqueda <iracoon2014@gmail.com>
2021-02-22 10:38:56 -0600

    Finished Calculator

---
+++ Proyecto Prueba 1/Assets/Scripts/Calculator.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Calculator : MonoBehaviour
+{
+	public float value1;
+	
+	public float value2;
+	
+	public bool add;
+	
+	public bool subtract;
+	
+	public bool multiply;
+	
+	public bool divide;
+	
+	
+	
+	
+	[HideInInspector]
+	public float result;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+	{
+		Application.targetFrameRate = 60;
+		Operate();
+    }
+    
+	void Operate()
+	{
+		if (add == true)
+		{
+			Add();
+			Debug.Log("Addition Result Is:" + result );
+		}
+		
+		if (subtract ==  true)
+		{
+			Subtract();
+		}
+		
+		if (multiply ==  true)
+		{
+			Multiply();
+		}
+		
+		if (divide == true)
+		{
+			Divide();
+			Debug.Log("Division Result Is:" + result );
+		}
+	
+	}
+	
+	public void Add()
+    {
+	    result = value1 + value2;
+    }
+    
+	public void Subtract()
+	{
+		result = value1 - value2;
+		
+	}
+	
+	public void Multiply()
+	{
+		result = value1 * value2;		
+	}
+	
+	public void Divide()
+	{
+		result = value1 / value2;
+	}
+    
+    
+}
